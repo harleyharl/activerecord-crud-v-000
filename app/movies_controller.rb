ca947f27200929fa3ef13b085fd61441453967b3
Harley Rayner <harleymteden@gmail.com>
2018-11-13 20:55:35 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -36,7 +36,6 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
     movie = Movie.new
     movie.title = args[:title]
     movie.release_date = args[:release_date]
-    movie.save
     # movie
     binding.pry
   end
