6a0367dd945535eed62719051f3b3a25cff5e10b
Harley Rayner <harleymteden@gmail.com>
2018-11-13 20:49:29 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -34,6 +34,7 @@ def can_be_created_in_a_block(args = {})
 
   Movie.create do |m|
     movie = Movie.new
+    binding.pry
     movie.title = args[:title]
     movie.release_date = args[:release_date]
     movie.save
