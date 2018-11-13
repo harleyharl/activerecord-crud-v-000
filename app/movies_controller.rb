14d6865e2b1d04bb816e31df84db97fcf48a19aa
Harley Rayner <harleymteden@gmail.com>
2018-11-13 21:15:07 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,10 +24,10 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
-
   Movie.create do |m|
-    # binding.pry
     movie = Movie.create(args)
+    movie.title == args[:title]
+    movie.release_date == args[:release_date]
   end
 end
 
