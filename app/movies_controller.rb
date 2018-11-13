e641314f9cb03b8d417e60837e0ac079fc2000b1
Harley Rayner <harleymteden@gmail.com>
2018-11-13 21:38:49 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -36,9 +36,7 @@ def can_get_the_first_item_in_the_database
 end
 
 def can_get_the_last_item_in_the_database
-  db_size = Movie.count
-  binding.pry
-  Movie.find(db_size)
+  Movie.last
 end
 
 def can_get_size_of_the_database
